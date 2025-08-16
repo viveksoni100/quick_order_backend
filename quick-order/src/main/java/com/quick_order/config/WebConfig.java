ab52133ab6c22de5cf228f1df5243bea2b82acc7
viveksoni100 <viveksoni100@gmail.com>
2025-08-16 11:39:40 +0530

    cors added

---
+++ quick-order/src/main/java/com/quick_order/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.quick_order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Allow your Angular app's origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
